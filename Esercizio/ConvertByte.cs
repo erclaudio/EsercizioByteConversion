939342831e8cb607a5a3ee8ba260db75ac080b3e
erclaudio <erfattone@gmail.com>
2024-05-23 17:02:55 +0100

    Add project files.

---
+++ Esercizio/ConvertByte.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio
+{
+    class ConvertByte
+    {
+        public void IntToByte(int integer1, int integer2 ) {
+            switch (integer1)
+            {
+                case var x when x > 255:
+                    Console.WriteLine($"Cannot convert first number '{integer1}' to byte!");
+                    if (integer2 > 255)
+                    {
+                        Console.WriteLine($"Cannot convert second number '{integer2}' to byte!\nTry again!");                        
+                    }
+                    else
+                    { Console.WriteLine($"Byte conversion second number: {(byte)integer2}\nTry again!"); }                    
+                    break;
+
+                case var x when x < 255:
+                    Console.WriteLine($"Byte conversion first number: {(byte)integer1}");
+                    if (integer2 > 255)
+                    {
+                        Console.WriteLine($"Cannot convert second number '{integer1}' to byte!\nTry again!");                        
+                    }
+                    else
+                    { Console.WriteLine($"Byte conversion second number: {(byte)integer2}\nTry again!"); }                    
+                    break;
+                default:
+                    break;
+            }
+
+        }
+            
+    }
+}
---
+++ Esercizio/Program.cs
@@ -0,0 +1,40 @@
+﻿using Esercizio;
+using System;
+using static Esercizio.ConvertByte;
+namespace CastingTypeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter a first number to convert to byte: ");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter a second number to convert to byte: ");
+            var second = Convert.ToInt32(Console.ReadLine());            
+            switch (first)
+            {
+                case var x when x > 255:
+                    Console.WriteLine($"Cannot convert first number '{first}' to byte!");
+                    if (second > 255)
+                    { Console.WriteLine($"Cannot convert second number '{second}' to byte!\nTry again!");
+                    Main(args); }
+                    else
+                    { Console.WriteLine($"Byte conversion second number: {(byte)second}\nTry again!"); }
+                    Main(args);  
+                    break;
+
+                case var x when x < 255:
+                    Console.WriteLine($"Byte conversion first number: {(byte)first}");
+                    if (second > 255)
+                    { Console.WriteLine($"Cannot convert second number '{second}' to byte!\nTry again!");
+                    Main(args); }
+                    else
+                    { Console.WriteLine($"Byte conversion second number: {(byte)second}\nTry again!"); }
+                    Main(args);                    
+                    break;
+                default:                    
+                    break;
+            }              
+         }
+    }
+}
